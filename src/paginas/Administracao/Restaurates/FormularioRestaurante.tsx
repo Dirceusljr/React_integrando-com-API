37b9b5727c059be42977967c46e377ce8942651b
Dirceusljr <dirjunior18@gmail.com>
2024-04-03 19:02:21 -0300

    aula 05

--- src/paginas/Administracao/Restaurates/FormularioRestaurante.tsx
+++ src/paginas/Administracao/Restaurates/FormularioRestaurante.tsx
@@ -41,11 +41,11 @@ const FormularioRestaurante = () => {
   };
 
   return (
-    <Box sx={{display: "flex", flexDirection: "column", alignItems: "center" }}>
+    <Box sx={{display: "flex", flexDirection: "column", alignItems: "center", flexGrow: 1 }}>
       <Typography component="h1" variant="h6">
         Formulário de Restaurantes
       </Typography>
-      <Box component="form" onSubmit={aoSubmeterForm}>
+      <Box component="form" onSubmit={aoSubmeterForm} sx={{width: '100%'}}>
         <TextField
           value={nomeRestaurante}
           onChange={(evento) => setNomeRestaurante(evento.target.value)}
